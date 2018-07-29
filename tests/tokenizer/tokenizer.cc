1218972763a253ea78660de8a725fe93bde0559a
uHOOCCOOHu <hooccooh1896@gmail.com>
2018-07-29 17:02:00 +0800

    Add tests for tokenizer.

---
+++ tests/tokenizer/tokenizer.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <lccpy/util/stream.h>
+#include <lccpy/parse/tokenizer.h>
+#include <lccpy/serialize/token.h>
+using namespace ccpy;
+using namespace ccpy::parse;
+using namespace ccpy::serialize;
+using namespace std;
+
+int main() {
+  auto iconsole = WrapIStream { cin };
+  auto tok = Tokenizer { iconsole };
+  auto oconsole = WrapOStream { cout };
+  auto base_ser = StructualSerializer { oconsole };
+  auto ser = TokenTreeSerializer { base_ser };
+
+  try {
+    while(auto c = tok.get()) {
+      ser.put(*c);
+      oconsole << "\n";
+    }
+    oconsole << "End\n";
+  } catch(StreamFailException e) {
+    oconsole << "Fail: " << e.what() << "\n";
+  } catch(StreamFatalException e) {
+    oconsole << "Fatal: " << e.what() << "\n";
+  }
+  return 0;
+}
